372745820e82617e1e45b1fa3c39e15a8af28aeb
Lukas Malkmus <lukas@axiom.co>
2021-03-01 19:13:35 +0100

    Fix link to examples in GoDoc

--- axiom/doc.go
+++ axiom/doc.go
@@ -27,5 +27,5 @@
 // signals and deadlines to requests. In case there is no context available,
 // then context.Background() can be used as a starting point.
 //
-// For more code samples, check out the https://github.com/axiomhq/axiom-go/tree/master/example directory.
+// For more code samples, check out the https://github.com/axiomhq/axiom-go/tree/main/examples directory.
 package axiom
