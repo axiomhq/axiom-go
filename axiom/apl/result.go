30f539c8af69f3292081df219cf7d97eb2bc4e12
Lukas Malkmus <lukas@axiom.co>
2021-11-30 09:53:42 +0100

    Fix examples

--- axiom/apl/result.go
+++ axiom/apl/result.go
@@ -5,8 +5,8 @@ import "github.com/axiomhq/axiom-go/axiom/query"
 // Result is the result of an APL query. It adds the APL query request alongside
 // the query result it created, making it a superset of `query.Result`
 type Result struct {
+	*query.Result
+
 	// Request is the APL query request that created the result.
 	Request *query.Query `json:"request"`
-
-	*query.Result
 }
