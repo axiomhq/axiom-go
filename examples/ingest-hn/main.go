c930b6a91bd76d945b15b3015182b47908049d66
Arne Bahlo <arne@axiom.co>
2022-08-07 17:59:10 +0200

    Move logic into generator functions for readability

--- examples/ingest-hn/main.go
+++ examples/ingest-hn/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"strconv"
 	"sync"
 
@@ -17,47 +16,45 @@ import (
 
 const BaseURL = "https://hacker-news.firebaseio.com"
 
-func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(),
-		os.Interrupt,
-		os.Kill,
-	)
-	defer cancel()
-
-	datasetName := os.Getenv("DATASET_NAME")
-	if datasetName == "" {
-		log.Fatal("Missing DATASET_NAME")
-	}
-
+func getMaxItemID() (uint64, error) {
 	res, err := http.Get(BaseURL + "/v0/maxitem.json")
 	if err != nil {
-		log.Fatal(fmt.Errorf("failed to get maxitem.json: %w", err))
+		return 0, fmt.Errorf("failed to get maxitem.json: %w", err)
 	}
+	defer res.Body.Close()
+
 	maxItemIDBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		res.Body.Close()
-		log.Fatal(fmt.Errorf("failed to read body: %w", err))
+		return 0, fmt.Errorf("failed to read body: %w", err)
 	}
-	res.Body.Close()
+
 	maxItemID, err := strconv.ParseUint(string(maxItemIDBytes), 10, 64)
 	if err != nil {
-		log.Fatal(fmt.Errorf("failed to parse maxitem.json: %w", err))
+		return 0, fmt.Errorf("failed to parse maxitem.json: %w", err)
 	}
 
-	workerWaitGroup := sync.WaitGroup{}
-	workQueue := make(chan uint64, 100)
-	eventChan := make(chan axiom.Event, 1000)
+	return maxItemID, nil
+}
+
+func generateIDs(max uint64) <-chan uint64 {
+	ch := make(chan uint64, 100)
 
 	go func() {
-		for i := uint64(0); i <= maxItemID; i++ {
-			select {
-			case <-ctx.Done():
-				log.Fatal(ctx.Err())
-			case workQueue <- i:
-			}
+		for i := uint64(0); i <= max; i++ {
+			ch <- i
 		}
 
-		close(workQueue)
+		close(ch)
+	}()
+
+	return ch
+}
+
+func fetchEvents(eventIDs <-chan uint64) <-chan axiom.Event {
+	eventChan := make(chan axiom.Event, 1000)
+
+	workerWaitGroup := sync.WaitGroup{}
+	go func() {
 		workerWaitGroup.Wait()
 		close(eventChan)
 	}()
@@ -66,13 +63,7 @@ func main() {
 		workerWaitGroup.Add(1)
 		go func() {
 			defer workerWaitGroup.Done()
-			for id := range workQueue {
-				select {
-				case <-ctx.Done():
-					log.Fatal(ctx.Err())
-				default:
-				}
-
+			for id := range eventIDs {
 				log.Printf("Fetching item %d\n", id)
 				res, err := http.Get(fmt.Sprintf("%s/v0/item/%d.json", BaseURL, id))
 				if err != nil {
@@ -91,12 +82,29 @@ func main() {
 		}()
 	}
 
+	return eventChan
+}
+
+func main() {
+	datasetName := os.Getenv("DATASET_NAME")
+	if datasetName == "" {
+		log.Fatal("Missing DATASET_NAME")
+	}
+
+	maxItemID, err := getMaxItemID()
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to get max item id: %w", err))
+	}
+
+	idChan := generateIDs(maxItemID)
+	eventChan := fetchEvents(idChan)
+
 	client, err := axiom.NewClient()
 	if err != nil {
 		log.Fatal(fmt.Errorf("failed to construct Axiom client: %w", err))
 	}
 
-	_, err = client.Datasets.IngestChannel(ctx, datasetName, eventChan, axiom.IngestOptions{})
+	_, err = client.Datasets.IngestChannel(context.TODO(), datasetName, eventChan, axiom.IngestOptions{})
 	if err != nil {
 		log.Fatal(fmt.Errorf("failed to ingest events: %w", err))
 	}
