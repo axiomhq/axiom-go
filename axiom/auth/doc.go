852c6075f1c7af191591e348758385b170513721
Lukas Malkmus <lukas@axiom.co>
2022-08-30 12:42:58 +0200

    doc(auth): improve package comment

--- axiom/auth/doc.go
+++ axiom/auth/doc.go
@@ -1,3 +1,3 @@
-// Package auth implements the authentication flow to login to Axiom in the
-// browser and retrieve a Personal Access Token.
+// Package auth implements the authentication flow to login to Axiom by visiting
+// a URL. A Personal Access Token is retrieved in exchange.
 package auth
