140ebe5400b04079988ebb218a3b417f94ba40d2
Arne Bahlo <arne@axiom.co>
2024-04-09 16:36:11 +0200

    fix(config): Allow xait- prefix for API tokens
    Signed-off-by: Arne Bahlo <arne@axiom.co>

--- internal/config/token.go
+++ internal/config/token.go
@@ -4,7 +4,7 @@ import "strings"
 
 // IsAPIToken returns true if the given token is an API token.
 func IsAPIToken(token string) bool {
-	return strings.HasPrefix(token, "xaat-")
+	return strings.HasPrefix(token, "xaat-") || strings.HasPrefix(token, "xait-")
 }
 
 // IsPersonalToken returns true if the given token is a personal token.
