03baf19783e2196af825c15d4bb4ca2ed8b3862a
Lukas Malkmus <lukas@axiom.co>
2025-01-27 12:17:28 +0100

    .github/workflows: replace deprecated 'axiom dataset info' with 'axiom query' to verify test

--- examples/zerolog/main.go
+++ examples/zerolog/main.go
@@ -19,10 +19,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer writer.Close()
 
 	l.Logger = zerolog.New(io.MultiWriter(writer, os.Stderr)).With().Logger()
 
 	l.Logger.Info().Str("mood", "hyped").Msg("This is awesome!")
-	l.Logger.Warn().Str("mood", "worried").Msg("This is no that awesome...")
+	l.Logger.Warn().Str("mood", "worried").Msg("This is not that awesome...")
 	l.Logger.Error().Str("mood", "depressed").Msg("This is rather bad.")
 }
